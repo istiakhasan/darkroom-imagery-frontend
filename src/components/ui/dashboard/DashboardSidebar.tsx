63c20e06b6dc6103c0c71cfeb499a920e27f58a3
istiakhasan <istiakhasan@gmail.com>
2023-10-14 13:41:16 +0600

    common component created

--- src/components/ui/dashboard/DashboardSidebar.tsx
+++ src/components/ui/dashboard/DashboardSidebar.tsx
@@ -4,25 +4,56 @@ import React, { useState } from "react";
 import {
   UserOutlined,
 } from "@ant-design/icons";
+import Link from "next/link";
 const DashboardSidebar = () => {
   const { Sider } = Layout;
   const [collapsed, setCollapsed] = useState(false);
   type MenuItem = Required<MenuProps>["items"][number];
-
+  const role:string="user"
   const items: MenuItem[] = [
     {
+      key: "home",
+      label: <Link href={"/home"}>Home</Link>,
+      icon:<UserOutlined />
+    },
+     {
       key: 1,
-      label: "Profile",
+      label: <Link href={"/profile"}>Profile</Link>,
       icon:<UserOutlined />
     },
     {
       key: 2,
-      label: "Services",
+      label: <Link href={`/user/booking-history`}>Booking History</Link>,
       icon:<UserOutlined />
     },
     {
       key: 3,
-      label: "Profile",
+      label: <Link href={`/admin/manage-user`}>Manage User</Link>,
+      icon:<UserOutlined />
+    },
+     {
+      key: 4,
+      label: <Link href={`/admin/manage-service`}>Manage Services</Link>,
+      icon:<UserOutlined />
+    },
+    {
+      key: 4,
+      label: <Link href={`/admin/manage-booking`}>Manage Booking</Link>,
+      icon:<UserOutlined />
+    },
+    {
+      key: 5,
+      label: <Link href={`/admin/manage-booking`}>Manage FAQ</Link>,
+      icon:<UserOutlined />
+    },
+    {
+      key: 6,
+      label: <Link href={`/admin/manage-blog`}>Manage Blog</Link>,
+      icon:<UserOutlined />
+    },
+    {
+      key: 7,
+      label: <Link href={`/admin/make-admin`}>Make Admin</Link>,
       icon:<UserOutlined />
     },
   ];
